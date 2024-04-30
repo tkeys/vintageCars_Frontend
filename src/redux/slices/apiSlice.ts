efcec6d800d072a5540f3732a03357a5020fbaf8
Theokeys <theolarsenn@gmail.com>
2024-04-30 13:27:13 +0300

    set headers

--- src/redux/slices/apiSlice.ts
+++ src/redux/slices/apiSlice.ts
@@ -12,8 +12,8 @@ const baseQuery = fetchBaseQuery({
       headers.set("authorization", `Bearer ${token}`);
       headers.set("Authorization", `Bearer ${token}`);
     }
-    /*   headers.append("origin", "Access-Control-Allow-Origin"); */
-    headers.append("Origin", "https://fs17-e-commerce-project.vercel.app/");
+    headers.set("origin", "Access-Control-Allow-Origin");
+
     headers.set("Content-Type", "application/json");
     return headers;
   },
